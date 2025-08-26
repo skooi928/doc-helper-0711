fe3d49348c1fb2e12ab05ddb9c86b77959a22794
SK <skooi866@gmail.com>
2025-08-26 17:08:26 +0800

    Initialized backend setup (Spring Boot with Maven, javasdk 21)

---
+++ backend/dochelper-backend/src/main/java/com/nezha/docs/dochelper_backend/DochelperBackendApplication.java
@@ -0,0 +1,13 @@
+package com.nezha.docs.dochelper_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DochelperBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DochelperBackendApplication.class, args);
+	}
+
+}
---
+++ backend/dochelper-backend/src/test/java/com/nezha/docs/dochelper_backend/DochelperBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nezha.docs.dochelper_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DochelperBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
